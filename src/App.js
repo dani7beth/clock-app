46caea8b78684ccd012a3025c399d0e569e0a7fe
dani7beth <danielledona28@gmail.com>
2020-12-09 11:52:01 -0700

    added a axios data function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-import { Container, Header } from 'semantic-ui-react';
+import { Button, Container, Header } from 'semantic-ui-react';
+import Data from './func/Data';
+import {useState} from 'react';
 
 function App() {
+  const [showData, setShowData] = useState(false);
   return (
     <Container >
       <Header as="h1">LifeCycle</Header>
+      {showData && <Data />}
+      <Button onClick={()=>setShowData(!showData)}>{showData ? "hide" : "Show"}</Button>
     </Container>
   );
 }
